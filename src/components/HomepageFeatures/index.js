fb7aaa5eac8b8243effd3fc9af6c789d0ee8bb01
Gabriel110 <gabrielcorreiadev@gmail.com>
2026-04-07 21:42:44 -0300

    colando themas e as linguaguens que eu sei

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,44 +4,38 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Java',
+    Svg: require('@site/static/img/java-original.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Java é uma linguagem de programação orientada a objetos criada pela Sun Microsystems e atualmente mantida pela Oracle.
+        Ela é multiplataforma, pois roda na JVM, permitindo executar o mesmo código em diferentes sistemas operacionais.
+        Java é amplamente utilizado em sistemas corporativos, APIs, aplicações Android e frameworks como Spring Boot.
+        É uma linguagem robusta, segura e muito presente no mercado de desenvolvimento de software.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Python',
+    Svg: require('@site/static/img/python-original.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Python é uma linguagem de programação de alto nível criada por Guido van Rossum, conhecida por sua sintaxe simples e fácil de aprender.
+        É muito utilizada em áreas como automação, ciência de dados, inteligência artificial e desenvolvimento web com frameworks como Django.
+        Python é multiplataforma, possui grande comunidade e é uma das linguagens mais populares do mundo atualmente.
       </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    )
   },
 ];
 
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--6' +
+      '')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className="text--justify padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
       </div>
@@ -53,7 +47,7 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className="row margin--sm">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
